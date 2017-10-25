7152a540330a539059c192927a613c4e11b29d3c
“grasper <32321160+feeling-tester@users.noreply.github.com>
2017-10-25 10:55:28 +0900

    First

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// var Account_s = [
+//     "@smallptarigan", "@issyo_ptarmigan", "@Bear_0821", "@ktoahkeii"
+// ];
+
+// var m = 11;
+// var msf="eeee";
+
+// var FrontKusoripu = "このサイトみて価値観変わったわ? まあ、クソリプは辞めないけどねw クソリプ稼いで落単した ";
+// var BackKusoripu = " の話なんだけど、無料だし１回読んでみるといいと思う。";
+
+// var msg = FrontKusoripu +
+//     // Account_s[document.AccountNameBox.Account.selectedIndex] +
+//     BackKusoripu;
+
+// var d = document.getElementById("special");
+// d.setAttribute("data-text", "ああああ");
+
+
