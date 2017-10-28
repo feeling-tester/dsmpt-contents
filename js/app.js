0c665fe9ca6ae3dcea2495c87a377c5f833f34c6
yuckey <yucky8phi@gmail.com>
2017-10-28 11:39:36 +0900

    view-port update

--- js/app.js
+++ js/app.js
@@ -45,6 +45,36 @@ function initMap() {
         markerEvent(i); // マーカーにクリックイベントを追加
         
     }
+
+
+    var GeolocationWindow = new google.maps.InfoWindow({map: map});
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            GeolocationWindow.setPosition(pos);
+            GeolocationWindow.setContent('Location found.');
+            map.setCenter(pos);
+        }, function() {
+            handleLocationError(true, GeolocationWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, GeolocationWindow, map.getCenter());
+    }
+
+
+
+
+
+
+
+    
 }
 var infoWindowOpenFlag = 0;
 
