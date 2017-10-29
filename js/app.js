b5551e421f27214b6736ce75b2b4b7f700d00e68
yuckey <yucky8phi@gmail.com>
2017-10-29 11:22:20 +0900

    hide error message

--- js/app.js
+++ js/app.js
@@ -80,10 +80,10 @@ function initMap() {
     }
     
     function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
+        //infoWindow.setPosition(pos);
+        //infoWindow.setContent(browserHasGeolocation ?
+          //                    'Error: The Geolocation service failed.' :
+          //                    'Error: Your browser doesn\'t support geolocation.');
     }
     
 }
